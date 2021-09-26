b9c334e7dc96035deb8d7b3f7078f0e8de1cd417
Kamino666 <516015417@qq.com>
2021-09-26 09:34:15 +0800

    feat: 模型训练文件

---
+++ VideoTransformer.py
@@ -0,0 +1,248 @@
+import torch
+import torch.nn as nn
+from torch.nn import Transformer
+from torch import Tensor
+from torch.utils.data import Dataset, DataLoader
+
+from transformers import BertModel, AutoTokenizer
+
+import math
+import pathlib as plb
+import random
+import json
+from tqdm import tqdm
+import numpy as np
+from timeit import default_timer as timer
+
+device = torch.device("cuda")
+
+
+class Opt:
+    batch_size = 16
+    bert_type = "bert-base-uncased"
+    enc_layer_num = 3
+    dec_layer_num = 3
+    head_num = 8
+    emb_dim = 768
+    hid_dim = 768
+    dropout = 0.1
+    lr = 1e-4
+    epoch_num = 50
+
+
+class MSRVTT(Dataset):
+    def __init__(self, video_feat_dir: str, annotation_file: str, tokenizer,
+                 random_seed: int = 1234, mode: str = "train"):
+        super(MSRVTT, self).__init__()
+        random.seed(random_seed)
+        self.tokenizer = tokenizer
+        # load video list
+        video_feat_dir = plb.Path(video_feat_dir)
+        self.video_feat_list = list(video_feat_dir.glob("*.npy"))
+        self.mode = mode
+
+        # load caption
+        if mode == "train" or "val" or "validate":
+            self.video2caption = {}
+            with open(annotation_file, encoding='utf-8') as f:
+                annotation = json.load(f)
+            self.video2split = {i["video_id"]: i["split"] for i in annotation["videos"]}
+            for cap in tqdm(annotation["sentences"], desc="Loading annotations"):
+                if self.video2split[cap["video_id"]] != mode:
+                    continue
+                if cap["video_id"] not in self.video2caption:
+                    self.video2caption[cap["video_id"]] = [cap["caption"]]
+                else:
+                    self.video2caption[cap["video_id"]].append(cap["caption"])
+
+    def __getitem__(self, index):
+        video_path = self.video_feat_list[index]
+        vid = video_path.stem
+        v_feat = torch.tensor(np.load(str(video_path)), dtype=torch.float)
+        if self.mode == "train" or "val" or "validate":
+            caption = random.choice(self.video2caption[vid])
+            caption = self.tokenizer(caption, padding=True, return_tensors="pt")
+            return v_feat, caption["input_ids"], caption["attention_mask"]
+        return v_feat
+
+    def __len__(self):
+        return len(self.video_feat_list)
+
+
+def collate_fn(data):
+    """
+    :param data:
+    :return tuple(N T E, N T):
+    """
+    if type(data[0]) is tuple:
+        feat_data = torch.stack([i[0] for i in data])
+        text_data = torch.stack([i[1] for i in data])
+        mask_data = torch.stack([i[2] for i in data]).to(torch.bool)
+        return feat_data, text_data, mask_data
+    else:
+        return torch.stack(data)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self,
+                 emb_size: int,
+                 dropout: float,
+                 maxlen: int = 5000):
+        super(PositionalEncoding, self).__init__()
+        den = torch.exp(- torch.arange(0, emb_size, 2) * math.log(10000) / emb_size)
+        pos = torch.arange(0, maxlen).reshape(maxlen, 1)
+        pos_embedding = torch.zeros((maxlen, emb_size))
+        pos_embedding[:, 0::2] = torch.sin(pos * den)
+        pos_embedding[:, 1::2] = torch.cos(pos * den)
+        pos_embedding = pos_embedding.unsqueeze(-2)
+
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('pos_embedding', pos_embedding)
+
+    def forward(self, token_embedding: Tensor):
+        # 广播机制加到每一个batch上
+        return self.dropout(token_embedding + self.pos_embedding[:token_embedding.shape[1], :])
+
+
+# Seq2Seq Network
+# [N,T,E] and [N,S,E] -> [N, len, vocab_size]
+class VideoTransformer(nn.Module):
+    def __init__(self,
+                 num_encoder_layers: int,
+                 num_decoder_layers: int,
+                 emb_size: int,
+                 nhead: int,
+                 bert_type: str,
+                 dim_feedforward: int = 512,
+                 dropout: float = 0.1):
+        super(VideoTransformer, self).__init__()
+        self.transformer = Transformer(d_model=emb_size,
+                                       nhead=nhead,
+                                       num_encoder_layers=num_encoder_layers,
+                                       num_decoder_layers=num_decoder_layers,
+                                       dim_feedforward=dim_feedforward,
+                                       dropout=dropout,
+                                       batch_first=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(bert_type)
+        self.generator = nn.Linear(emb_size, self.tokenizer.vocab_size)
+        self.tgt_tok_emb = BertModel.from_pretrained(bert_type)
+        self.positional_encoding = PositionalEncoding(emb_size, dropout=dropout)
+
+    def forward(self,
+                src: Tensor,
+                trg: Tensor,
+                src_mask: Tensor,
+                tgt_mask: Tensor,
+                src_padding_mask: Tensor,
+                tgt_padding_mask: Tensor,
+                memory_key_padding_mask: Tensor):
+        src_emb = self.positional_encoding(self.src_tok_emb(src))
+        tgt_emb = self.positional_encoding(self.tgt_tok_emb(trg))
+        outs = self.transformer(src_emb, tgt_emb, src_mask, tgt_mask, None,
+                                src_padding_mask, tgt_padding_mask, memory_key_padding_mask)
+        return self.generator(outs)
+
+    def encode(self, src: Tensor, src_mask: Tensor):
+        return self.transformer.encoder(self.positional_encoding(src), src_mask)
+
+    def decode(self, tgt: Tensor, memory: Tensor, tgt_mask: Tensor):
+        return self.transformer.decoder(
+            self.positional_encoding(self.tgt_tok_emb(tgt)),
+            memory,
+            tgt_mask
+        )
+
+
+def generate_square_subsequent_mask(sz):
+    mask = (torch.triu(torch.ones((sz, sz), device=device)) == 1).transpose(0, 1)
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    return mask
+
+
+def train_epoch(model, optimizer, opt: Opt, tokenizer: AutoTokenizer):
+    model.train()
+    losses = 0
+    train_iter = MSRVTT(r"./data/msrvtt-train-feats",
+                        r"./data/MSRVTT-annotations/train_val_videodatainfo.json",
+                        tokenizer=tokenizer)
+    train_dataloader = DataLoader(train_iter, batch_size=opt.batch_size, collate_fn=collate_fn, shuffle=True)
+
+    for src, tgt, tgt_padding_mask in tqdm(train_dataloader):
+        src = src.to(device)
+        tgt = tgt.to(device)
+        tgt_padding_mask = tgt_padding_mask.to(device)
+
+        tgt_input = tgt[:, :-1]  # N T-1
+        tgt_mask = generate_square_subsequent_mask(tgt.shape[1])
+
+        logits = model(src, tgt_input,
+                       tgt_mask=tgt_mask, tgt_padding_mask=tgt_padding_mask,
+                       src_mask=None, src_padding_mask=None)  # N T-1 vocab_szie
+
+        optimizer.zero_grad()
+
+        tgt_out = tgt[:, 1:]  # N T-1
+        loss = loss_fn(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1))
+        loss.backward()
+
+        optimizer.step()
+        losses += loss.item()
+
+    return losses / len(train_dataloader)
+
+
+def evaluate(model, opt: Opt, tokenizer: AutoTokenizer):
+    model.eval()
+    losses = 0
+
+    val_iter = MSRVTT(r"./data/msrvtt-validate-feats",
+                      r"./data/MSRVTT-annotations/train_val_videodatainfo.json",
+                      tokenizer=tokenizer, mode="val")
+    val_dataloader = DataLoader(val_iter, batch_size=opt.batch_size, collate_fn=collate_fn)
+
+    for src, tgt, tgt_padding_mask in val_dataloader:
+        src = src.to(device)
+        tgt = tgt.to(device)
+        tgt_padding_mask = tgt_padding_mask.to(device)
+
+        tgt_input = tgt[:, :-1]  # N T-1
+        tgt_mask = generate_square_subsequent_mask(tgt.shape[1])
+        with torch.no_grad:
+            logits = model(src, tgt_input,
+                           tgt_mask=tgt_mask, tgt_padding_mask=tgt_padding_mask,
+                           src_mask=None, src_padding_mask=None)  # N T-1 vocab_szie
+
+            tgt_out = tgt[1:, :]  # N T-1
+            loss = loss_fn(logits.reshape(-1, logits.shape[-1]), tgt_out.reshape(-1))
+        losses += loss.item()
+
+    return losses / len(val_dataloader)
+
+
+if __name__ == "__main__":
+    opt = Opt()
+    transformer = VideoTransformer(num_encoder_layers=opt.enc_layer_num,
+                                   num_decoder_layers=opt.dec_layer_num,
+                                   emb_size=opt.emb_dim,
+                                   nhead=opt.head_num,
+                                   bert_type=opt.bert_type,
+                                   dropout=opt.dropout,
+                                   dim_feedforward=opt.hid_dim)
+    tokenizer = AutoTokenizer.from_pretrained(opt.bert_type)
+    pad_id = tokenizer.convert_tokens_to_ids("[PAD]")
+
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+
+    transformer = transformer.to(device)
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=pad_id)
+    optimizer = torch.optim.Adam(transformer.parameters(), lr=opt.lr)
+
+    for epoch in range(1, opt.epoch_num + 1):
+        start_time = timer()
+        train_loss = train_epoch(transformer, optimizer, opt, tokenizer)
+        end_time = timer()
+        val_loss = evaluate(transformer, opt, tokenizer)
+        print(f"Epoch: {epoch}, Train loss: {train_loss:.3f},"
+              f" Val loss: {val_loss:.3f}, "f"Epoch time = {(end_time - start_time):.3f}s")
