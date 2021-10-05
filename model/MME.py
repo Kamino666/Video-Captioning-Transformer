feat: 完成MMT的训练代码 fix: 修复了test模式下没有softmax的bug fix: 修复了memory_key_padding没有指定的bug

model/MME.py
@@ -1,5 +1,6 @@
 import torch
-from torch.nn import Module, Linear, Dropout, ModuleList, AdaptiveMaxPool1d, AdaptiveAvgPool1d
+import torch.nn as nn
+from torch.nn import Module, Linear, Dropout, AdaptiveMaxPool1d, AdaptiveAvgPool1d
 from torch import Tensor
 from .Embedding import PositionalEmbedding
 
@@ -26,33 +27,69 @@ class GlobalAggregation(Module):
             return self.agg(x.transpose(1, 2)).transpose(1, 2)
 
 
-class MultiModalEmbedding(Module):
-    def __init__(self, d_model: int, feat_dims: list, dropout: float, agg_method: str = "avgpooling"):
+class ExpertEmbedding(Module):
+    def __init__(self, expert_num):
+        super(ExpertEmbedding, self).__init__()
+        self.expert_num = expert_num
+        self.emb_value = 0
+
+    def step(self):
+        self.emb_value += 1
+
+    def forward(self, x):
         """
-        输入N种feature，输出将这些feature映射到d_model维度的特征以及一个额外的Global特征
+        :param x: N, T+1, d_model
+        :return:
+        """
+        return x + self.emb_value
+
+
+class MultiModalEmbedding(Module):
+    def __init__(self, d_model: int, feat_dims: dict, dropout: float, agg_method: str = "avgpooling",
+                 device=torch.device('cuda')):
+        """
+        输入λ种特征，分别进行Input Embedding，再得到Global特征，再将local和Global一起进行Positional Embedding和Expert Embedding
         Global特征可以用GRU、Pooling等方式得到
-        :param d_model: Transformer的emd_dim
-        :param feat_dims: 输入的K种特征的维度列表
+        :param d_model: Transformer的维度
+        :param feat_dims: 输入的K种特征的维度字典
         :param agg_method: 进行全局特征提取的方式 maxpooling/avgpooling/GRU/biGRU
         :param dropout: PositionalEmbedding的Dropout
         """
         super(MultiModalEmbedding, self).__init__()
-        self.projs = ModuleList([Linear(d, d_model) for d in feat_dims])
-        self.agg = GlobalAggregation(method=agg_method)
-        self.expert_emb = list(range(10))
-        self.pe = PositionalEmbedding(d_model, dropout)
+        self.device = device
+        assert set(feat_dims.keys()).issubset({"scene", "motion"})
+        self.input_embeddings = nn.ModuleDict({
+            k: Linear(v, d_model) for k, v in feat_dims.items()
+        })
+        self.aggregates = nn.ModuleDict({
+            k: GlobalAggregation(method=agg_method) for k in feat_dims.keys()
+        })
+        self.positional_embedding = PositionalEmbedding(d_model, dropout)
+        self.expert_embedding = ExpertEmbedding(len(feat_dims))
 
-    def forward(self, feats):
-        video_embeddings = []
-        for expert, feat, proj in zip([self.expert_emb, feats, self.projs]):
-            v_feat = proj(feat)  # N T E
-            agg_v_feat = self.agg(v_feat)  # N 1 E
-
-            v_feat = v_feat + torch.ones_like(v_feat) * expert
-            agg_v_feat = agg_v_feat + torch.ones_like(agg_v_feat) * expert
-
-            video_embeddings.append(
-                torch.cat([agg_v_feat, self.pe(v_feat)], dim=1)  # N T+1 E
+    def forward(self, feats_dict: dict, feats_padding_mask_dict: dict):
+        features, lengths, feats_padding_masks = [], [], []
+        for feat_name, feats, padding_masks in zip(feats_dict.keys(), feats_dict.values(), feats_padding_mask_dict.values()):
+            # B T E -> B T M  # M meas d_model
+            feats = self.input_embeddings[feat_name](feats)
+            # B 1 M
+            global_feat = self.aggregates[feat_name](feats)
+            # B T M + B 1 M -> B T+1 M
+            feats = torch.cat([global_feat, feats], dim=1)
+            # Positional Embedding
+            feats = self.positional_embedding(feats)
+            # Expert Embedding
+            feats = self.expert_embedding(feats)
+            self.expert_embedding.step()
+            features.append(feats)
+            lengths.append(feats.shape[1])
+            # B T -> B T+1
+            false_ts = (torch.zeros((feats.shape[0], 1), dtype=torch.long) == 1).to(self.device)
+            feats_padding_masks.append(
+                torch.cat([false_ts, padding_masks], dim=1)
             )
-        return video_embeddings
+        features = torch.cat(features, dim=1)
+        feats_padding_masks = torch.cat(feats_padding_masks, dim=1)
+        return features, feats_padding_masks, lengths
+
 

model/model.py
@@ -48,32 +48,31 @@ class VideoTransformer(nn.Module):
 
     def forward(self,
                 src: Tensor,
-                tgt: Tensor,
+                tgt: Optional[Tensor, List[Tensor]],
                 src_mask: Tensor,
-                tgt_mask: Tensor,
+                tgt_mask: Optional[Tensor, List[Tensor]],
                 src_padding_mask: Tensor,
-                tgt_padding_mask: Tensor,
-                memory_key_padding_mask: Tensor = None):
+                tgt_padding_mask: Optional[Tensor, List[Tensor]],):
         src_emb = self.positional_encoding(self.src_to_emb(src))  # src: torch.Size([16, 768, 20])
 
         # 如果是单个caption，则直接transformer
         if type(tgt) == Tensor:
             tgt_emb = self.positional_encoding(self.tgt_to_emb(tgt))
             outs = self.transformer(src_emb, tgt_emb, src_mask, tgt_mask, None,
-                                    src_padding_mask, tgt_padding_mask, memory_key_padding_mask)
+                                    src_padding_mask, tgt_padding_mask, src_padding_mask)
             return self.generator(outs)
         # 如果是多个caption，则依次decode出结果
         elif type(tgt) == list:
             # 先得到encoder的输出
-            memory = self.transformer.encoder(src_emb, mask=src_mask, src_key_padding_mask=src_padding_mask)
+            memory = self.encoder(src, mask=src_mask, src_key_padding_mask=src_padding_mask)
             # 再一个一个视频decode
             tgts, tgt_masks, tgt_padding_masks = tgt, tgt_mask, tgt_padding_mask
             outputs = []
-            for tgt, tgt_mask, tgt_padding_mask in zip(tgts, tgt_masks, tgt_padding_masks):
+            for tgt, tgt_mask, tgt_padding_mask in zip([tgts, tgt_masks, tgt_padding_masks]):
                 tgt_emb = self.positional_encoding(self.tgt_to_emb(tgt))
-                output = self.transformer.decoder(tgt_emb, memory, tgt_mask=tgt_mask,
+                output = self.decoder(tgt_emb, memory, tgt_mask=tgt_mask,
                                       tgt_key_padding_mask=tgt_padding_mask,
-                                      memory_key_padding_mask=memory_key_padding_mask)
+                                      memory_key_padding_mask=src_padding_mask)
                 output = self.generator(output)
                 outputs.append(output)
             return outputs
@@ -102,7 +101,7 @@ class MMVideoTransformer(nn.Module):
     def __init__(self,
                  num_encoder_layers: int,
                  num_decoder_layers: int,
-                 feat_sizes: List[int],
+                 feat_dims: dict,
                  d_model: int,
                  nhead: int,
                  bert_type: str = "bert-base-uncased",
@@ -113,9 +112,7 @@ class MMVideoTransformer(nn.Module):
                  agg_method: str = "avgpooling",
                  ):
         super(MMVideoTransformer, self).__init__()
-        feat_num = len(feat_sizes)
-        self.feat_num = feat_num
-        self.emb_size = d_model
+        self.d_model = d_model
         self.device = device
 
         tokenizer = AutoTokenizer.from_pretrained(bert_type)
@@ -123,47 +120,40 @@ class MMVideoTransformer(nn.Module):
         vocab_size = tokenizer.vocab_size
         self.tgt_to_emb = nn.Embedding(vocab_size, d_model, padding_idx=pad_id)
         self.positional_encoding = PositionalEmbedding(d_model, dropout=dropout)
-        self.mme = MultiModalEmbedding(d_model, feat_sizes, dropout, agg_method=agg_method)
 
-        self.encoders = nn.ModuleList([
-            nn.TransformerEncoder(nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout, activation, batch_first=True),
-                                  num_encoder_layers,
-                                  nn.LayerNorm(d_model)) for _ in range(feat_num)
-        ])
+        self.mme = MultiModalEmbedding(d_model, feat_dims, dropout, agg_method=agg_method)
 
-        decoder_layer = nn.TransformerDecoderLayer(d_model, nhead, dim_feedforward, dropout, activation, batch_first=True)
-        decoder_norm = nn.LayerNorm(d_model)
-        self.decoder = nn.TransformerDecoder(decoder_layer, num_decoder_layers, decoder_norm)
+        self.mmt_encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout, activation, batch_first=True),
+            num_encoder_layers,
+            nn.LayerNorm(d_model)
+        )
+        self.mmt_decoder = nn.TransformerDecoder(
+            nn.TransformerDecoderLayer(d_model, nhead, dim_feedforward, dropout, activation,batch_first=True),
+            num_decoder_layers,
+            nn.LayerNorm(d_model)
+        )
 
         self.generator = nn.Linear(d_model, self.tokenizer.vocab_size)
 
     def forward(self,
-                srcs: List[Tensor],
+                feats_dict: dict,
+                feats_padding_mask_dict: dict,
                 tgt: Tensor,
-                src_masks: List[Tensor],
                 tgt_mask: Tensor,
-                src_padding_masks: List[Tensor],
-                tgt_padding_mask: Tensor,
-                memory_key_padding_mask: Tensor = None):
-        memories = []
-        for src, src_mask, src_padding_mask in zip(srcs, src_masks, src_padding_masks):
-            src = self.positional_encoding(src)  # src: N T E
-            memory = self.encoder(src, mask=src_mask, src_key_padding_mask=src_padding_mask)  # memory N T E
-            memories.append(memory)
-        video_embeddings = self.mme(memories)  # List[(N, T+1, E)]
-        video_embeddings = torch.cat(video_embeddings, dim=1)  # N K(T+1) E
+                tgt_padding_mask: Tensor):
+        # video features
+        features, feats_padding_masks, lengths = self.mme(feats_dict, feats_padding_mask_dict)
+        memories = self.mmt_encoder(
+            features, mask=None, src_key_padding_mask=feats_padding_masks
+        )
 
-        tgt_emb = self.positional_encoding(self.tgt_to_emb(tgt))
-        output = self.decoder(tgt_emb, video_embeddings, tgt_mask=tgt_mask, memory_mask=None,
-                              tgt_key_padding_mask=tgt_padding_mask,
-                              memory_key_padding_mask=memory_key_padding_mask)
-        return self.generator(output)  # N S E -> N S vocab_size
+        # caption embeddings
+        caption_emb = self.positional_encoding(self.tgt_to_emb(tgt))
+        output = self.mmt_decoder(
+            caption_emb, memories, tgt_mask=tgt_mask, tgt_key_padding_mask=tgt_padding_mask,
+            memory_mask=None, memory_key_padding_mask=feats_padding_masks
+        )
 
-    def encode(self, srcs):
-        memories = []
-        for src in srcs:
-            src = self.positional_encoding(src)  # src: N T E
-            memory = self.encoder(src)  # memory N T E
-            memories.append(memory)
-        video_embeddings = self.mme(memories)  # List[(N, T+1, E)]
-        video_embeddings = torch.cat(video_embeddings, dim=1)  # N K(T+1) E
+        return self.generator(output)
+

train.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
 from transformers import AutoTokenizer
@@ -20,33 +21,33 @@ device = torch.device("cuda")
 
 class Opt:
     # data
-    train_feat_dir = r"data/MSRVTT-CLIP-FEATURES/train_feats"
+    train_feat_dir = r"data/msrvtt_resnet152_fps3_feats/train"
     train_annotation_path = r"./data/MSRVTT-annotations/train_val_videodatainfo.json"
-    val_feat_dir = r"data/MSRVTT-CLIP-FEATURES/val_feats"
+    val_feat_dir = r"data/msrvtt_resnet152_fps3_feats/val"
     val_annotation_path = r"./data/MSRVTT-annotations/train_val_videodatainfo.json"
     raw_video_dir = r"data/MSRVTT_trainval"
     # train
-    batch_size = 8
-    lr = 5e-4
-    max_len = 30
-    learning_rate_patience = 10
-    early_stopping_patience = 30
+    batch_size = 32
+    lr = 1e-4
+    max_len = 20
+    learning_rate_patience = 5
+    early_stopping_patience = 10
     # model
     bert_type = "bert-base-uncased"
-    enc_layer_num = 2
-    dec_layer_num = 2
+    enc_layer_num = 4
+    dec_layer_num = 4
     head_num = 8
-    feat_size = 512
-    emb_dim = 768
-    hid_dim = 768
+    feat_size = 2048
+    emb_dim = 512
+    hid_dim = 2048
     dropout = 0.3
-    epoch_num = 300
+    epoch_num = 30
     use_bert = False
     # save & load
     save_freq = 50
     load_model = None
     model_save_dir = "./checkpoint"
-    _extra_msg = "MSRVTT&CLIP&gda"  # Dataset|Bert|pretrained
+    _extra_msg = "MSRVTT&R152"  # Dataset|Bert|pretrained
     training_name = f"b{batch_size}_lr{str(lr)[2:]}_dp{str(dropout).replace('.', '')}_emb{emb_dim}_e{enc_layer_num}" \
                     f"_d{dec_layer_num}_hd{head_num}_hi{hid_dim}_{_extra_msg}"
 
@@ -149,6 +150,7 @@ def greedy_decode(model, src, max_len, start_symbol, end_symbol):
         tgt_mask = (generate_square_subsequent_mask(ys.shape[1]).type(torch.bool)).to(device)  # t, t
         out = model.decode(ys, memory, tgt_mask)
         prob = model.generator(out[:, -1])  # vocab_size
+        prob = F.softmax(prob)
         _, next_word = torch.max(prob, dim=1)
         next_word = next_word.item()
 
@@ -271,18 +273,18 @@ if __name__ == "__main__":
                                    verbose=True,
                                    path=os.path.join(opt.model_save_dir, f"{opt.training_name}_earlystop.pth"))
     # visulize & log
-    writer = LogWriter(f"./log/gda/{opt.training_name}")
+    writer = LogWriter(f"./log/resnet/{opt.training_name}")
 
     # dataloader
-    train_iter = MSRVTT(opt.train_feat_dir, opt.train_annotation_path, tokenizer=tokenizer, return_all_captions=True)
-    train_dataloader = DataLoader(train_iter, batch_size=opt.batch_size, collate_fn=multi_cap_collate_fn, shuffle=True)
+    train_iter = MSRVTT(opt.train_feat_dir, opt.train_annotation_path, tokenizer=tokenizer)
+    train_dataloader = DataLoader(train_iter, batch_size=opt.batch_size, collate_fn=collate_fn, shuffle=True)
     val_iter = MSRVTT(opt.val_feat_dir, opt.val_annotation_path, tokenizer=tokenizer, mode="validate")
     val_dataloader = DataLoader(val_iter, batch_size=opt.batch_size, collate_fn=collate_fn)
 
     # train
     for epoch in range(1 + st_epoch, opt.epoch_num + 1 + st_epoch):
         start_time = timer()
-        train_loss = train_epoch_gda(transformer, optimizer, train_dataloader)
+        train_loss = train_epoch(transformer, optimizer, train_dataloader)
         end_time = timer()
 
         val_loss = evaluate(transformer, val_dataloader)